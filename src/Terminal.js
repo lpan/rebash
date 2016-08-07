27f6a7fc780befcc96607d5c78f8c332e1e6565c
Lawrence Pan <lawrencefeipan@gmail.com>
2016-08-07 00:03:11 -0400

    basic filesystem idea

--- src/Terminal.js
+++ src/Terminal.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import styles from './style.css';
+import React, {Component, PropTypes} from 'react';
 
-const Terminal = () => (
-  <div className={styles.cont}>
-    <p className={styles.blueText}>Hello</p>
-  </div>
-);
+class Terminal extends Component {
+  constructor(props) {
+    super(props);
+
+    const {initialPath, directories, files} = props;
+    this.state = {
+      // A list of commands <String> can be access with up-arrow
+      history: [],
+      // A list of tuples -> [:command, :output] that are visible on the screen
+      visibles: [],
+      // A list of directories represented as absolute path
+      directories,
+      // look up a file's content using its absolute path as key
+      files,
+      // an array representation of current path
+      currentPath: initialPath,
+    };
+  }
+
+  render() {
+    return (
+      <div>Lmao</div>
+    );
+  }
+}
+
+Terminal.propTypes = {
+  // a list of directory paths
+  directories: PropTypes.arrayOf(PropTypes.string),
+  // file content mapped to file absolute path
+  files: PropTypes.objectOf(PropTypes.string),
+  initialPath: PropTypes.arrayOf(PropTypes.string),
+  commands: PropTypes.objectOf(PropTypes.func),
+};
+
+Terminal.defaultProps = {
+  initialPath: ['/'],
+  files: {},
+  directories: [],
+};
 
 export default Terminal;
