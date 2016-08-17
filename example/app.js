05b629e897d8aa11297ca14e908029d895bbb99a
Lawrence Pan <lawrencefeipan@gmail.com>
2016-08-17 15:24:45 -0400

    add props to example

--- example/app.js
+++ example/app.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Terminal from 'react-term';
+import {head} from 'ramda';
+
+const commands = {
+  'open-window': args => { alert(head(args)); },
+};
+
+const files = {
+  '/home/lawrence/greet.txt': 'Hello',
+};
+
+const directories = [
+  '/home/lpan',
+  '/etc/nginx/',
+];
 
 ReactDOM.render(
-  <Terminal />,
+  <Terminal
+    files={files}
+    directories={directories}
+    commands={commands}
+  />,
   document.getElementById('root')
 );
