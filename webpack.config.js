bddc9f92027cf372302c176068e3a7b20af35633
Lawrence Pan <lawrencefeipan@gmail.com>
2017-01-20 21:04:26 -0500

    ready to host on gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
 const webpack = require('webpack');
 const path = require('path');
+const {mergeWith, concat} = require('ramda');
 
-const SRC_PATH = path.join(__dirname, 'example', 'app.js');
-const BUILD_PATH = path.join(__dirname, 'example', 'build');
+const SRC_PATH = path.join(__dirname, 'docs', 'example', 'app.js');
+const BUILD_PATH = path.join(__dirname, 'docs');
 
-module.exports = {
+const ENV = process.env.NODE_ENV;
+
+const devel = {
   devtool: 'cheap-module-eval-source-map',
 
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+};
+
+const prod = {
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false,
+      },
+    }),
+  ],
+};
+
+const base = {
   entry: SRC_PATH,
   output: {
     path: BUILD_PATH,
@@ -26,13 +49,20 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: [/node_modules/, /.+\.config.js/],
-        loader: 'babel',
+        loader: 'babel-loader',
       },
     ],
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(ENV),
+      },
+    }),
   ],
 };
+
+const config = ENV === 'production' ? prod : devel;
+
+module.exports = mergeWith(concat, base, config);
